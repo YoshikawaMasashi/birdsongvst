832b69c2c2c031f30ed957ac7cd39d0b744c486d
Masashi Yoshikawa <earnest.marshi@gmail.com>
2023-11-16 14:32:31 +0900

    update

--- birdsongvst/src/pi/sine.rs
+++ birdsongvst/src/pi/sine.rs
@@ -59,10 +59,10 @@ fn ode(
 }
 
 pub struct SineOscillator {
-    phase: f64, // オシレータの進行具合。sin(x)のxを保持してる。
-    freq: f64,  // 鳴らしている音の周波数。
-    pitch: f64, // ピッチベンド幅を保持しておくフィールド。
-
+    fs: usize,
+    gamma: f64,
+    ovfs: usize,
+    
     t: usize,
 
     v: [f64; 6],
@@ -70,6 +70,8 @@ pub struct SineOscillator {
     alpha: Vec<f64>,
     beta: Vec<f64>,
     envelope: Vec<f64>,
+    pi: Vec<f64>,
+    pb: Vec<f64>,
 }
 
 impl SineOscillator {
@@ -80,17 +82,20 @@ impl SineOscillator {
         let beta = bytes_to_f64_array(beta_bytes.to_vec());
         let envelope_bytes = std::include_bytes!("../../../rust_synth/data/envelope.raw");
         let envelope = bytes_to_f64_array(envelope_bytes.to_vec());
+        let ovfs = 20;
+        let tmax = alpha.len() * ovfs - 1;
         Self {
-            phase: 0.0,
-            freq: 440.0,
-            pitch: 0.0,
-
+            fs: 44100,
+            gamma: 40000.0,
+            ovfs,
             t: 0,
             v: [1e-4 * 1e2, 1e-4 * 1e1, 1e-4, 1e-4, 1e-4, 1e-4],
 
             alpha,
             beta,
             envelope,
+            pi: vec![0.0; tmax],
+            pb: vec![0.0; tmax],
         }
     }
 }
@@ -109,6 +114,69 @@ impl Triggered for SineOscillator {
 
 impl AudioProcessor<f64> for SineOscillator {
     fn process(&mut self, sample_rate: f64) -> f64 {
-        0.0
+        let (c, L, r, Ch, MG, MB, RB, Rh) = (
+            343.0, 0.025, 0.65, 1.43e-10, 20.0, 10000.0, 5000000.0, 24000.0,
+        );
+        let tmax = self.alpha.len() * self.ovfs - 1;
+        let dt = 1.0 / (self.ovfs * self.fs) as f64;
+
+        for i in 0..self.ovfs {
+            if self.t >= tmax {
+                return 0.0;
+            }
+
+            let k1 = ode(
+                self.t, self.v, &mut self.pi, &mut self.pb, &self.alpha, &self.beta, self.gamma, &self.envelope, c, L, r, Ch, MG, MB, RB, Rh,
+                dt, self.ovfs,
+            );
+    
+            let v2_vec: Vec<f64> = self.v
+                .iter()
+                .zip(k1.iter())
+                .map(|(v_, k1_)| v_ + dt / 2.0 * k1_)
+                .collect();
+            let mut v2: [f64; 6] = [0.0; 6];
+            v2.copy_from_slice(&v2_vec);
+            let k2 = ode(
+                self.t, v2, &mut self.pi, &mut self.pb, &self.alpha, &self.beta, self.gamma, &self.envelope, c, L, r, Ch, MG, MB, RB, Rh,
+                dt, self.ovfs,
+            );
+    
+            let v3_vec: Vec<f64> = self.v
+                .iter()
+                .zip(k2.iter())
+                .map(|(v_, k2_)| v_ + dt / 2.0 * k2_)
+                .collect();
+            let mut v3: [f64; 6] = [0.0; 6];
+            v3.copy_from_slice(&v3_vec);
+            let k3 = ode(
+                self.t, v3, &mut self.pi, &mut self.pb, &self.alpha, &self.beta, self.gamma, &self.envelope, c, L, r, Ch, MG, MB, RB, Rh,
+                dt, self.ovfs,
+            );
+    
+            let v4_vec: Vec<f64> = self.v
+                .iter()
+                .zip(k3.iter())
+                .map(|(v_, k3_)| v_ + dt * k3_)
+                .collect();
+            let mut v4: [f64; 6] = [0.0; 6];
+            v4.copy_from_slice(&v4_vec);
+            let k4 = ode(
+                self.t, v4, &mut self.pi, &mut self.pb, &self.alpha, &self.beta, self.gamma, &self.envelope, c, L, r, Ch, MG, MB, RB, Rh,
+                dt, self.ovfs,
+            );
+    
+            self.v = [
+                self.v[0] + dt * (2.0 * (k2[0] + k3[0]) + k1[0] + k4[0]) / 6.0,
+                self.v[1] + dt * (2.0 * (k2[1] + k3[1]) + k1[1] + k4[1]) / 6.0,
+                self.v[2] + dt * (2.0 * (k2[2] + k3[2]) + k1[2] + k4[2]) / 6.0,
+                self.v[3] + dt * (2.0 * (k2[3] + k3[3]) + k1[3] + k4[3]) / 6.0,
+                self.v[4] + dt * (2.0 * (k2[4] + k3[4]) + k1[4] + k4[4]) / 6.0,
+                self.v[5] + dt * (2.0 * (k2[5] + k3[5]) + k1[5] + k4[5]) / 6.0,
+            ];
+
+            self.t += 1;
+        }
+        RB * self.v[5]
     }
 }
